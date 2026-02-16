627db65a371d795832a0cddf01c8bcb2b0860f1f
Rohansalal <rohansalal33@gmail.com>
2026-02-17 03:10:46 +0530

    add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
-
-import Script from 'next/script' // Add this import
+import Script from 'next/script'
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
@@ -83,24 +82,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-P2NV42PX');
+        `}
+      </Script>
       <body className={`font-sans antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P2NV42PX"
+            height="0" 
+            width="0" 
+            style={{display:'none',visibility:'hidden'}}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <Analytics />
-        {/* Google Analytics */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-H05GZG03HD"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics-config" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-H05GZG03HD');
-          `}
-        </Script>
-        {/* End Google Analytics */}
         <Script id="crisp-chat-script" strategy="lazyOnload">
           {`
             window.$crisp=[];
